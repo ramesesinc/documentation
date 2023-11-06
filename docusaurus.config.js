218ab62cd256405a9419543b9b7deb0b35a28de4
Kevin Abaquita <nivekbusinesses@gmail.com>
2023-11-06 11:15:45 +0800

    changed url to username

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/entracslogo.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com',
+  url: 'https://NiveKing.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/etracsdocs/',
@@ -20,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'NiveKing', // Usually your GitHub org/user name.
   projectName: 'etracsdocs', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  // deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
